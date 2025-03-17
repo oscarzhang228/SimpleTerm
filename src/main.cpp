f3690d85a5c5200de2d27f679eb3785409130397
Oscar Zhang <oscarzhang228@gmail.com>
2025-03-16 21:05:28 -0400

    remove boilerplate from code crafters
    llljj
    
    k

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
+#include <set>
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
-
-  // Uncomment this block to pass the first stage
-  std::cout << "$ ";
+  std::set<std::string> validCommands;
 
   std::string input;
   std::getline(std::cin, input);
+  
+  if (!validCommands.contains(input)) {
+    std::cout << "Invalid command" << std::endl;
+  }
+
 }
